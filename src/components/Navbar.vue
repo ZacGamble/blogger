b763de5457f9aa08c8f80bfbc645a080ed84fe3d
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-04-28 14:01:03 -0600

    front page styling

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,9 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+  <nav class="navbar navbar-expand-lg navbar-dark text-dark bg-primary px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+      <div class="d-flex align-items-center">
+          <i class="mdi mdi-email mdi-36px text-dark"></i>
+          <h3 class="ps-2 text-dark">Blogger</h3>
       </div>
     </router-link>
     <button
@@ -17,16 +18,6 @@
       <span class="navbar-toggler-icon" />
     </button>
     <div class="collapse navbar-collapse" id="navbarText">
-      <ul class="navbar-nav me-auto">
-        <li>
-          <router-link
-            :to="{ name: 'About' }"
-            class="btn text-success lighten-30 selectable text-uppercase"
-          >
-            About
-          </router-link>
-        </li>
-      </ul>
       <!-- LOGIN COMPONENT HERE -->
       <Login />
     </div>
