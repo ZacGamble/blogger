3d01f776c454eb62d3311bbb5ddc3ebf634027a8
Joshua Wagstaff-Leon <wagstaffleon.joshua@gmail.com>
2022-04-28 16:54:16 -0600

    Blog form skeleton

---
+++ src/components/Modal.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="modal" tabindex="-1">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <slot name="modal-header"></slot>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <slot name="modal-body"></slot>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
